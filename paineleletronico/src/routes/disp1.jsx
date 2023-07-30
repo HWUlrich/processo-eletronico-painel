f74b1615004e5460c981766f6ea9d0c88773c668
unknown <hansfisica@gmail.com>
2023-07-29 22:15:24 -0300

    codigo das páginas disp1

--- paineleletronico/src/routes/disp1.jsx
+++ paineleletronico/src/routes/disp1.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useState, useEffect } from 'react';
 import './Disp.css';
 import blogFetchPar from '../axios/configPar';
@@ -6,14 +6,19 @@ import blogFetchPar from '../axios/configPar';
 const Disp1 = () => {
 
   const [parl, setParl] = useState([]);
+  const [parlSig, setParlSig] = useState([]);
 
   const getParl = async () => {
     
     try {
       const response = await blogFetchPar.get("/parlamentar");
-      
+      const response1 = await blogFetchPar.get("/partido");
+
       const data = response.data.results;
       setParl(data);
+
+      const data1 = response1.data.results;
+      setParlSig(data1);
 
     } catch (error) {
       console.log(error);
@@ -23,25 +28,33 @@ const Disp1 = () => {
   }
 
   useEffect(() => {
-    getParl();
+    getParl();    
   }, []);
 
-  //if(data.ativo === true) {
   return (
-    <div className='par'>      
-      {parl?.length === 0 ? (<p>Carregando...</p>) : (
+    <div className='par'>
+      <h1>Parlamentares - CMNF</h1>
+      {parl?.length === 0 ? (<p>Carregando...</p>) : (        
         parl?.map((parla) => (
           <div className="parl" key={parla.id}>
             <h1>{parla.nome_parlamentar}</h1>
             <h2>{parla.sexo}</h2>
-            <p>{parla.ativo}</p>            
+            {parlSig?.length === 0 ? (<p>Carregando...</p>) : (        
+              parlSig?.map((parla) => (
+                <div className="parl" key={parla.id}>            
+                  <p>{parla.sigla}</p>            
+                </div>
+        )
+        )
+      )}            
           </div>
         )
         )
-        )}
+      )}  
+    
     </div>
   );
-  //}
+
 }
 
 export default Disp1;
