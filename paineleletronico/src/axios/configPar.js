d58f673fd9ef83c3fee37fb2489fc4cd3f9a1d2f
unknown <hansfisica@gmail.com>
2023-08-04 21:12:51 -0300

    App, NavBar, Index, Disp css e configPar

--- paineleletronico/src/axios/configPar.js
+++ paineleletronico/src/axios/configPar.js
@@ -7,4 +7,13 @@ const blogFetchPar = axios.create({
     }
 });
 
+const blogFetchSes = axios.create({
+    baseURL: "https://sapl.novafriburgo.rj.leg.br/api/sessao-plenaria",
+    headers: {
+        "content-type": "application/json"
+    }
+});
+
+
 export default blogFetchPar;
+export default blogFetchSes;
