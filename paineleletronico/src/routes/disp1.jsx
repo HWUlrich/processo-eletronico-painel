ba130d25ecfc3dffd6e7dd9b53bc4b33f430af46
unknown <hansfisica@gmail.com>
2023-08-10 21:36:26 -0300

    Códigos das telas e css

--- paineleletronico/src/routes/disp1.jsx
+++ paineleletronico/src/routes/disp1.jsx
@@ -47,10 +47,13 @@ const Disp1 = () => {
           <div className="parl" key={parla.id}>                         
             <div className='parl-1'>
               <h1>{parla.nome_parlamentar}</h1> 
-            </div>            
+            </div>
             <div className='parl-2'>
               <h2>{parla.partido}</h2>
             </div>
+            <div className='parl-4'>
+              <h2>Presente</h2>
+            </div>           
             <div className='parl-3'>
               <h2>voto</h2>
             </div>
