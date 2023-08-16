50c8e77c53a90682fb96afb3e2841fb94254a1f2
unknown <hansfisica@gmail.com>
2023-08-15 22:14:45 -0300

    Código Disp1 e configSes

--- paineleletronico/src/routes/disp4.jsx
+++ paineleletronico/src/routes/disp4.jsx
@@ -2,33 +2,32 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './Disp.css';
 import axios from 'axios';
+import blogFetchSes from '../axios/configSes';
 
 
 const Disp4 = () => {  
 
-  const [sess, setSess] = useState([]);    
+  const [ses, setSes] = useState([]);    
 
   const getSes = async () => {
 
         
     try {
-      const url = `https://sapl.novafriburgo.rj.leg.br/api/sessao-plenaria`;
-      const response = await axios.get(url);
-      
+      const response = await blogFetchSes.get('?page=69&page_size=10');
+
+      //  'https://sapl.novafriburgo.rj.leg.br/api/sessao-plenaria/?page=69&page_size=10'      
      
       const pag = response.data.pagination.total_pages;
-      console.log(pag);
-      const key = Object.keys(response);
-      const lastKey = key[key.length - 1];
+      //console.log(pag);
+      //const key = Object.keys(response);
+      //console.log(key);
+      //const lastKey = key[key.length - 1];
+      //console.log(lastKey);
+      //const lastItem = response[lastKey];
+      // console.log(lastItem);      
       
-      
-      const data = response.data.results.slice(-1);
-      
-     
-
-      console.log(data);      
-      setSess(data);
-      
+      const data = response.data.results;      
+      setSes(data);      
 
     } catch (error) {
       console.log(error);
@@ -43,8 +42,8 @@ const Disp4 = () => {
 
   return (
     <div className='painel'>      
-      {sess?.length === 0 ? (<p>Carregando Painel...</p>) : (        
-        sess?.map((sessao) => (                    
+      {ses?.length === 0 ? (<p>Carregando Painel...</p>) : (        
+        ses?.map((sessao) => (                    
           <div className="painel-0" key={sessao.id}>                         
             <div className='painel-1'>
               <h1>{sessao.txtTituloReuniao}</h1> 
