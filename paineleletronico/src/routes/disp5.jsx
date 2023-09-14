d42b74f17be767968c4528925192db5fb98b9349
unknown <hansfisica@gmail.com>
2023-09-13 22:44:00 -0300

    codigo NavBar.css disp.css e disp5

--- paineleletronico/src/routes/disp5.jsx
+++ paineleletronico/src/routes/disp5.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import Disp1 from './disp1';
+import Disp2 from './disp2';
+import Disp3 from './disp3';
+import Disp4 from './disp4';
 
-const disp5 = () => {
-  
-  return (
-    <div>
+const Disp5 = () => {
 
+  //document.body.classList.toggle('dark-theme');
+ 
+  return (    
+    <div className='mesaDir'>
+      <div className='mesa'>
+      <Disp1 />
+      <Disp2 />
+      <Disp3 />
+      </div>   
+      <Disp4 />  
     </div>
-  )
+  );
 }
 
-export default disp5;
+export default Disp5;
