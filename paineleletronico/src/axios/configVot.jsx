ba6d17be53bd2c4bca7175a6dfdd8a15f0b0f6e7
HWUlrich <hansfisica@gmail.com>
2024-02-08 14:50:38 -0300

    alteração header configVot

--- paineleletronico/src/axios/configVot.jsx
+++ paineleletronico/src/axios/configVot.jsx
@@ -7,7 +7,7 @@ const aPIFetchVot = axios.create({
         "content-language": "pt-br", 
         //"content-length": 4954, 
         "content-type": "application/json",
-        "access-Control-Allow-Origin": "*",
+        "access-Control-Allow-Origin": "http://localhost:5173",
         "access-control-allow-headers": "Access-Control-Allow-Origin,XMLHttpRequest,Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With "
     }
 });
