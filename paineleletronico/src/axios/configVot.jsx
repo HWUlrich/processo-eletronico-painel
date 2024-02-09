2f6d112fea283a3a40b9adebaee8e4fe5d5558d1
HWUlrich <hansfisica@gmail.com>
2024-02-09 14:52:54 -0300

    Acerto na politica de CORS Policy

--- paineleletronico/src/axios/configVot.jsx
+++ paineleletronico/src/axios/configVot.jsx
@@ -2,12 +2,10 @@ import axios from 'axios';
 
 const aPIFetchVot = axios.create({
     baseURL: "https://sapl.novafriburgo.rj.leg.br/api/sessao/votoparlamentar/",
-    //timeout:  8000,
     headers: {       
-        "content-language": "pt-br", 
-        //"content-length": 4954, 
+        "content-language": "pt-br",         
         "content-type": "application/json",
-        "access-Control-Allow-Origin": "http://localhost:5173",
+        "access-Control-Allow-Origin": "*",
         "access-control-allow-headers": "Access-Control-Allow-Origin,XMLHttpRequest,Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With "
     }
 });
